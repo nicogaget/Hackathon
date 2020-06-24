497a25e93f04c3d93d52bcb040350b1cf1ca37de
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-06-25 01:17:59 +0200

    RDV à moitié

---
+++ src/Form/RdvType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Rdv;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RdvType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('isActive')
+            ->add('rdvOrder')
+            ->add('message')
+            ->add('adress')
+            ->add('practitioner')
+            ->add('patient')
+            ->add('creneau')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Rdv::class,
+        ]);
+    }
+}
