dcf6157e9d9c34f48c5c43b9f0c42beccad2c988
Guillaume <actix2013@gmail.com>
2020-06-24 23:44:22 +0200

    Create fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,15 +3,23 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\
 
 class AppFixtures extends Fixture
 {
+    const NB_PRACT = 1;
+    const NB_CLIENT = 10;
     public function load(ObjectManager $manager)
     {
+        $typeDoctor = new Type
+        
         // $product = new Product();
         // $manager->persist($product);
-
+       for($i = 0 ; $i < $this::NB_PRACT ; $i++) {
+           $aPract = new User
+       }
+        
         $manager->flush();
     }
 }
