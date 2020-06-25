a173826d58fec814cfae8df513634ad2d3c598d8
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-06-25 11:16:52 +0200

    Formulaire ok

--- src/Controller/RdvController.php
+++ src/Controller/RdvController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Rdv;
+use App\Entity\User;
 use App\Form\RdvType;
 use App\Repository\RdvRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,19 +17,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class RdvController extends AbstractController
 {
     /**
-     * @Route("/", name="rdv_index", methods={"GET"})
+     * @Route("/", name="rdv_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
      */
-    public function index(RdvRepository $rdvRepository): Response
-    {
-        return $this->render('rdv/index.html.twig', [
-            'rdvs' => $rdvRepository->findAll(),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="rdv_new", methods={"GET","POST"})
-     */
-    public function new(Request $request): Response
+    public function index(Request $request): Response
     {
         $rdv = new Rdv();
         $form = $this->createForm(RdvType::class, $rdv);
@@ -36,59 +29,21 @@ class RdvController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy(["id" => 1]);
+            $rdv->setIsActive(1);
+            $rdv->setPatient($user);
+
             $entityManager->persist($rdv);
             $entityManager->flush();
 
-            return $this->redirectToRoute('rdv_index');
+            return $this->redirectToRoute('rdv_new');
         }
 
         return $this->render('rdv/new.html.twig', [
             'rdv' => $rdv,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="rdv_show", methods={"GET"})
-     */
-    public function show(Rdv $rdv): Response
-    {
-        return $this->render('rdv/show.html.twig', [
-            'rdv' => $rdv,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="rdv_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Rdv $rdv): Response
-    {
-        $form = $this->createForm(RdvType::class, $rdv);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('rdv_index');
-        }
-
-        return $this->render('rdv/edit.html.twig', [
-            'rdv' => $rdv,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="rdv_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Rdv $rdv): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$rdv->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($rdv);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('rdv_index');
     }
 }
