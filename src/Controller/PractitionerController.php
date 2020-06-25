a629f75f8603740cceaea677cb124072c6838319
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-06-25 16:29:08 +0200

    Liste RDV

--- src/Controller/PractitionerController.php
+++ src/Controller/PractitionerController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Rdv;
 use App\Services\GeocodingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +15,21 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class PractitionerController extends AbstractController
 {
+    /**
+     * @Route("/", name="practitioner_index")
+     * @param GeocodingService $geocoding
+     * @return Response
+     */
+    public function index ()
+    {
+        $rdv=$this->getDoctrine()
+            ->getRepository(RDV::class)
+            ->findBy(["practitioner" => 35]);
+        return $this->render('practitioner/index.html.twig', [
+            'rdvs' => $rdv,
+        ]);
+    }
+
     /**
      * @Route("/list", name="practitioner_list")
      * @param GeocodingService $geocoding
