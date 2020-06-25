0afb1672c483701992a6d416a92daea3bbbf2c90
Nico <n.gaget@gmail.com>
2020-06-26 00:40:16 +0200

    correction

--- src/Controller/PatientController.php
+++ src/Controller/PatientController.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 
 /**
